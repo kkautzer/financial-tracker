56e5561ecb0c0acad7eec26e45b9937aa2f595e9
Keagan Kautzer <kkautzer05@gmail.com>
2025-05-16 12:45:45 -0500

    Add Default Values to Add Transaction Modal

--- client-web/src/components/app/components/TransactionForm.jsx
+++ client-web/src/components/app/components/TransactionForm.jsx
@@ -1,9 +1,9 @@
-export default function TransactionForm(props) {
+export default function TransactionForm({isIncome = false, handleSubmission = () => {}, modalId = '' }) {
     return <>
-        <dialog id={props.modalId} className='modal'>
+        <dialog id={modalId} className='modal'>
             <div className='modal-box max-w-5xl'>
                 <form method='dialog'><button className="text-xl btn btn-sm btn-circle btn-ghost absolute right-5 top-5">✕</button></form>
-                <h3 className='font-bold text-2xl mb-2'>Add {(props.isIncome === 'true' ? "Income" : "Expense")}</h3>
+                <h3 className='font-bold text-2xl mb-2'>Add {(isIncome ? "Income" : "Expense")}</h3>
                 <form method='dialog'>
                     <input className='text-xl my-1' type='text' placeholder="Category"/>
                     <br/>
@@ -11,7 +11,7 @@ export default function TransactionForm(props) {
                     <br/>
                     <input className='text-xl my-1' type='text' placeholder="Amount"/>
                     <br/>
-                    <button className="btn btn-primary mt-2 mr-1" onClick={props.handleSubmission}>Add</button>
+                    <button className="btn btn-primary mt-2 mr-1" onClick={handleSubmission}>Add</button>
                     <button className="btn btn-gray-300 mt-2 ml-1">Cancel</button>
                 </form>
             </div>
