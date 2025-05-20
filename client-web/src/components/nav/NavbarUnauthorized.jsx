472d9653467a5564a496e5498fc41aad0522d75f
Keagan Kautzer <kkautzer05@gmail.com>
2025-05-19 22:47:34 -0500

    Basic Responsivity for Unauthorized Navigation Bar

--- client-web/src/components/nav/NavbarUnauthorized.jsx
+++ client-web/src/components/nav/NavbarUnauthorized.jsx
@@ -1,12 +1,12 @@
 import { NavLink } from "react-router";
 
 export default function Navbar() {
-    return <div className="navbar bg-primary text-neutral-content">
+    return <div className="sm:navbar bg-primary text-neutral-content">
         <div className="flex-1">
             <NavLink to='/' className="btn btn-ghost primary-case text-xl">MyFinanceTracker</NavLink>
         </div>
         <div className="flex-none">
-            <ul className="menu menu-horizontal px-1 primary-case">
+            <ul className="menu sm:menu-horizontal px-1 primary-case">
                 <li><NavLink to='/home'>Home</NavLink></li>
                 <li><NavLink to='/about'>About</NavLink></li>
                 <li><NavLink to='/login'>Login / Register</NavLink></li>
