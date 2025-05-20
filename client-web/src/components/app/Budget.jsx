c323067bd417f20ed3b546e5a262450f93834c4b
Keagan Kautzer <kkautzer05@gmail.com>
2025-05-19 22:36:00 -0500

    Basic Responsive Navbar (**Needs Improvement)

--- client-web/src/components/app/Budget.jsx
+++ client-web/src/components/app/Budget.jsx
@@ -1,3 +1,10 @@
 export default function Budget() {
-    return <p>This is the budget page!!</p>
+    // to make a partially-filled background, use a linear gradient as seen in the following example
+    const pctFill = 35;
+    return <div 
+        style={{backgroundImage: `linear-gradient(to right, green ${pctFill}%, transparent ${pctFill}%)`}}
+        className="bg-base-100"
+    >
+        <p>This is the budget page!!</p>
+    </div>
 }
